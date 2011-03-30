26c553e7b1699ebf9909c91107968026e8d18248
Jeremy Walworth <jeremy@jeremywalworth.com>
2011-03-30 00:11:28 -0400

    now it can save and fetch values from Riak

---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -v -X PUT -T jquery.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/jquery.js
+curl -v -X PUT -T riak-javascript-client/riak.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/riak.js
+curl -v -X PUT -T riak-javascript-client/json2.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/json2.js
+curl -v -X PUT -T index.html -H "Content-type: text/html" http://localhost:8098/riak/vault/index.html
