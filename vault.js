3e6f171d353f10b48eea218a6cfed050072bf1ef
Jeremy Walworth <jeremy@jeremywalworth.com>
2011-10-31 23:40:08 -0400

    cleared out old stuff

--- vault.js
+++ vault.js
@@ -1,43 +1,29 @@
-function find() {
+function list_keys(){
     client = new RiakClient();
-    client.bucket('passwords', function(b){get_object(b);});
+    var bucket = new RiakBucket('passwords', client);
+    bucket.keys(function(keys){
+        var data = {'keys': keys.sort()};
+        var template = [['div', {'id':'passphrase'},
+                            ['a', {'href':'#/new'}, 'New Account'],
+                            ['table',
+                                ['tr',
+                                    ['td', 'Passphrase:'],
+                                    ['td',
+                                        ['input', {'type':'password', 'id':'secret'}]
+                                    ]
+                                ]
+                            ]
+                        ],
+                        ['div',
+                            data.keys.map(function(text){
+                                return ['p', ['a', {'href': '#/fetch/'+text}, text]];
+                            })
+                        ]
+        ];
+        $('#content').html(microjungle(template));
+    });
 }
 
-function save(){
-    client = new RiakClient();
-    client.bucket('passwords', function(b){get_or_new_object(b);});
-    $('#errors').text('Password stored');
-}
-
-function get_or_new_object(bucket){
-    bucket.get_or_new($('input#key').val(), function(status, o){save_object(o);});
-}
-
-function save_object(object){
-    encrypted = GibberishAES.enc($('input#value').val(), $('input#secret').val());
-    object.body = encrypted;
-    object.contentType = 'text/plain';
-    object.store();
-    $('input#value').val(encrypted);
-}
-
-function get_object(bucket){
-    bucket.get($('input#key').val(), function(s,o){get_value(o);});
-}
-
-function get_value(object){
-    try {
-        decrypted = GibberishAES.dec(object.body, $('input#secret').val());
-        $('input#value').val(decrypted);
-        $('#errors').text("");
-    }
-    catch(e) {
-        $('#errors').text("Error decrypting, passphrase may be incorrect");
-        $('input#value').val("");
-    }
-}
-
-//---------------------BEGIN NEW STUFF
 function fetch(key){
     client = new RiakClient();
     client.bucket('passwords', function(bucket){
@@ -75,47 +61,6 @@ function fetch(key){
     });
 }
 
-function store(){
-    client = new RiakClient();
-    client.bucket('passwords', function(bucket){
-        bucket.get_or_new($('input#key').val(), function(status, object){
-            record = {
-                'username':$('input#username').val(),
-                'password':$('input#password').val(),
-                'notes':$('textarea#notes').val()
-            };
-            encrypted = GibberishAES.enc(JSON.stringify(record), $('input#secret').val());
-            object.body = encrypted;
-            object.contentType = 'text/plain';
-            object.store();
-            $('#errors').text('Account info encrypted and stored ');
-        });
-    });
-
-    window.location = '#/list_keys';
-}
-
-function list_keys(){
-    client = new RiakClient();
-    var bucket = new RiakBucket('passwords', client);
-    bucket.keys(function(keys){
-        var data = {'keys': keys.sort()};
-        var template = [['div', {'id':'passphrase'},
-                            ['a', {'href':'#/new'}, 'New Account'],
-                            ['table',
-                                ['tr',
-                                    ['td', 'Passphrase:'],
-                                    ['td',
-                                        ['input', {'type':'password', 'id':'secret'}]]]]],
-                        ['div',
-                            data.keys.map(function(text){
-                                return ['p', ['a', {'href': '#/fetch/'+text}, text]];
-                            })
-                        ]];
-        $('#content').html(microjungle(template));
-    });
-}
-
 function new_item(){
     var template = [['table',
                         ['tr',
@@ -140,6 +85,26 @@ function new_item(){
     ]];
     $('#content').html(microjungle(template));
 
+}
+
+function store(){
+    client = new RiakClient();
+    client.bucket('passwords', function(bucket){
+        bucket.get_or_new($('input#key').val(), function(status, object){
+            record = {
+                'username':$('input#username').val(),
+                'password':$('input#password').val(),
+                'notes':$('textarea#notes').val()
+            };
+            encrypted = GibberishAES.enc(JSON.stringify(record), $('input#secret').val());
+            object.body = encrypted;
+            object.contentType = 'text/plain';
+            object.store();
+            $('#errors').text('Account info encrypted and stored ');
+        });
+    });
+
+    window.location = '#/list_keys';
 }
 
 $(function() {
