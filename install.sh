dee77d78f9ae61fdc99d36c41662764dde668a3e
jwalworth <jwalworth@hasuda.(none)>
2011-11-27 14:58:02 -0500

    added ability to specify server and bucket as args on the install script

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
-curl -v -X PUT -T jquery.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/jquery.js
-curl -v -X PUT -T riak.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/riak.js
-curl -v -X PUT -T json2.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/json2.js
-curl -v -X PUT -T gibberish-aes.min.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/gibberish-aes.min.js
-curl -v -X PUT -T microjungle.min.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/microjungle.min.js
-curl -v -X PUT -T SS.min.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/SS.min.js
-curl -v -X PUT -T vault.js -H "Content-type: text/javascript" http://localhost:8098/riak/vault/vault.js
-curl -v -X PUT -T index.html -H "Content-type: text/html" http://localhost:8098/riak/vault/index.html
-curl -v -X PUT -T vault.css -H "Content-type: text/html" http://locahost:8098/riak/vault/vault.css
+#!/bin/sh
+if [ -n "$1" ] ; then
+  node=$1
+else
+  node="127.0.0.1:8098"
+fi
+if [ -n "$2" ] ; then
+  bucket=$2
+else
+  bucket="vault"
+fi
+curl -v -X PUT -T jquery.js -H "Content-type: text/javascript" http://$node/riak/$bucket/jquery.js
+curl -v -X PUT -T riak.js -H "Content-type: text/javascript" http://$node/riak/$bucket/riak.js
+curl -v -X PUT -T json2.js -H "Content-type: text/javascript" http://$node/riak/$bucket/json2.js
+curl -v -X PUT -T gibberish-aes.min.js -H "Content-type: text/javascript" http://$node/riak/$bucket/gibberish-aes.min.js
+curl -v -X PUT -T microjungle.min.js -H "Content-type: text/javascript" http://$node/riak/$bucket/microjungle.min.js
+curl -v -X PUT -T SS.min.js -H "Content-type: text/javascript" http://$node/riak/$bucket/SS.min.js
+curl -v -X PUT -T vault.js -H "Content-type: text/javascript" http://$node/riak/$bucket/vault.js
+curl -v -X PUT -T index.html -H "Content-type: text/html" http://$node/riak/$bucket/index.html
+curl -v -X PUT -T vault.css -H "Content-type: text/html" http://$node/riak/$bucket/vault.css
